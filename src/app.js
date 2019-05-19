38a8dee8292a741f8b9ca52d9bfdafa44795ef21
liaozhen <2388189399@qq.com>
2019-05-19 21:47:53 +0800

    添加input组件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './button'
 import ButtonGroup from './buttonGroup'
+import Input from './input'
 Vue.component('gButton', Button)
 Vue.component('gButtonGroup', ButtonGroup)
+Vue.component('gInput',Input)
 new Vue({
     el: '#app',
     data() {
@@ -14,105 +16,3 @@ new Vue({
         }
     }
 })
-
-// 单元测试
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect = chai.expect
-{
-    const Constructor = Vue.extend(Button)
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const vm =  new Constructor({
-        propsData: {
-            type: 'primary',
-            icon: 'loading',
-            loading: true
-        }
-    })
-    vm.$mount(div)
-    const classNames = vm.$el.className.split(' ')
-    const use = vm.$el.querySelector('use')
-    const svg = vm.$el.querySelector('svg')
-    expect(classNames.indexOf('g-button-primary')).to.equal(1)
-    expect(svg.className.baseVal.split(' ').indexOf('icon-loading')).to.equal(1)
-    expect(use.getAttribute('xlink:href')).to.equal('#i-loading')
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-{
-    const Constructor = Vue.extend(Button)
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const vm =  new Constructor({
-        propsData: {
-            type: 'danger',
-            icon: 'loading',
-            iconPosition: 'left',
-        }
-    })
-    vm.$mount(div)
-    console.log(vm.$el.classList)
-    console.log(typeof vm.$el.className)
-    const classNames = vm.$el.className.split(' ')
-    console.log(classNames)
-    const use = vm.$el.querySelector('use')
-    const svg = vm.$el.querySelector('svg')
-    const { order } = window.getComputedStyle(svg)
-    console.log(svg)
-    expect(classNames.indexOf('g-button-danger')).to.equal(1)
-    // expect(svg.className).to.equal('icon-loading')
-    expect(use.getAttribute('xlink:href')).to.equal('#i-loading')
-    expect(order).to.equal('1')
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-{
-    const Constructor = Vue.extend(Button)
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const vm =  new Constructor({
-        propsData: {
-            type: 'danger',
-            icon: 'setting',
-            iconPosition: 'left',
-            size: 'large',
-            disabeld: true
-        }
-    })
-    vm.$mount(div)
-    console.log(vm.$el.classList)
-    console.log(typeof vm.$el.className)
-    const classNames = vm.$el.className.split(' ')
-    console.log(classNames)
-    const use = vm.$el.querySelector('use')
-    const svg = vm.$el.querySelector('svg')
-    const button = vm.$el.querySelector('button')
-    console.log(button)
-       const { order } = window.getComputedStyle(svg)
-    expect(classNames.indexOf('g-button-danger')).to.equal(1)
-    expect(classNames.indexOf('g-button-large')).to.equal(2)
-    // expect(svg.className).to.equal('icon-loading')
-    expect(use.getAttribute('xlink:href')).to.equal('#i-setting')
-    expect(order).to.equal('1')
-    // expect(button.getAttribute('disabeld')).to.equal('disabeld')
-}
-
-{
-    const Constructor = Vue.extend(Button)
-    const vm =  new Constructor({
-        propsData: {
-            type: 'danger',
-            icon: 'setting',
-            iconPosition: 'left',
-        }
-    })
-    vm.$mount()
-    let spy = chai.spy(function() {})
-    vm.$on('click', spy)
-    vm.$el.click()
-    expect(spy).to.have.been.called()
-}
---
+++ src/input.vue
@@ -0,0 +1,167 @@
+<template>
+    <div :class="['g-input', {'g-input-group': $slots.prepend || $slots.append}, {'g-input-prefix': prefixIcon}, {'g-input-suffix': suffixIcon}]">
+        <span class="g-input-prepend" v-if="$slots.prepend">
+            <slot name="prepend"></slot>
+        </span>
+        <span class="g-input-prefix-icon" v-if="prefixIcon">
+            <g-icon :icon-name="prefixIcon"></g-icon>
+        </span>
+        <input class="g-input-inner"
+               :placeholder="placeholder" 
+               :value="value"
+               :disabled="disabled"
+               :class="[{[`${size}-input`]: size}, {'g-input-inner-disabled': disabled}]"
+               @change="this.$emit('change', $event)"
+               @input="this.$emit('change', $event)"
+               @focus="this.$emit('focus', $event)"
+               @blur="this.$emit('blur', $event)"/>
+        <span class="g-input-append" v-if="$slots.append">
+            <slot name="append"></slot>
+        </span>
+        <span class="g-input-suffix-icon" v-if="suffixIcon">
+            <g-icon :icon-name="suffixIcon"></g-icon>
+        </span>
+    </div>
+</template>
+
+<script>
+    import gIcon from './icon.vue'
+    export default {
+        name: 'gInput',
+        data() {
+            return {
+
+            }
+        },
+        props: {
+            value: {
+                type: String,
+                default: ''
+            },
+            placeholder: {
+                type: String,
+                default: ''
+            },
+            size: {
+                type: String,
+                default: 'middle'
+            },
+            suffixIcon: {
+                type: String,
+                default: ''
+            },
+            prefixIcon: {
+                type: String,
+                default: 0
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        },
+        mounted() {
+            console.log(this.$slots)
+        },
+        components: {
+            gIcon
+        }
+    }
+</script>
+
+<style lang="scss">
+    $height: 32px;
+    $color: rgba(0,0,0,0.65);
+    $large-height: 40px;
+    $small-height: 24px;
+    $append-bg-color: #fafafa;
+    $border-color: #d9d9d9;
+    .g-input {
+        display: inline-block;
+        font-size: 14px;
+        width: 100%;
+        position: relative;
+        .g-input-inner {
+            height: $height;
+            border: 1px solid $border-color;
+            border-radius: 4px;
+            padding: 4px 10px;
+            color: $color;
+            background-color: #fff;
+            box-sizing: border-box;
+            width: 100%;
+            transition: all .3s;
+            font-size: 14px;
+            &.large-input {
+                height: $large-height;
+            }
+            &.small-input {
+                height: $small-height;
+            }
+            &.g-input-inner-disabled {
+                background-color: #f5f7fa
+            }
+            &:focus {
+                border-color: #40a9ff;
+                box-shadow: 0 0 0 2px rgba(24,144,255,0.2);
+            }
+        }
+        &.g-input-group {
+            display: table;
+            .g-input-inner {
+                display: table-cell;
+                vertical-align: middle;
+                border-top-left-radius: 0;
+                border-bottom-left-radius: 0;
+                border-top-right-radius: 0;
+                border-bottom-right-radius: 0;
+            }
+        }
+        &.g-input-prefix {
+            .g-input-inner {
+                padding-left: 30px;
+            }
+        }
+        &.g-input-suffix {
+            .g-input-inner {
+                padding-right: 30px;
+            }
+        }
+        .g-input-prepend, .g-input-append {
+            // height: 100%;
+            padding: 4px 10px;
+            background-color: $append-bg-color;
+            border: 1px solid $border-color;
+            color: $color;
+            font-weight: normal;
+            display: table-cell;
+            vertical-align: middle;
+            border-radius: 4px;
+            font-size: 14px;
+        }
+        .g-input-prepend {
+            border-right: 0px;
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+        }
+        .g-input-append {
+            border-left: 0px;
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+        .g-input-prefix-icon, .g-input-suffix-icon {
+            position: absolute;
+            height: 100%;
+            top: 0;
+            line-height: 100%;
+            color: red;
+            line-height: 40px;
+            font-size: 16px;
+        }
+        .g-input-prefix-icon {
+            left: 10px;
+        }
+        .g-input-suffix-icon {
+            right: 10px;
+        }
+    }
+</style>
