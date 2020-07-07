48f36e7e02851b3b0235c89a5f826d6e50b02b35
liaozhen <zhen.liao@baobeituan.com>
2020-07-07 17:59:42 +0800

    开始编写nav组件文档

--- docs/.vuepress/components/nav/demo1.vue
+++ docs/.vuepress/components/nav/demo1.vue
@@ -5,35 +5,8 @@
             <g-nav-item name="about">关于</g-nav-item>
             <g-sub-nav name="contact">
                 <template slot="title">联系方式</template>
-                <g-nav-item name="qq">QQ</g-nav-item>
+                <g-nav-item name="QQ">QQ</g-nav-item>
                 <g-nav-item name="weixin">微信</g-nav-item>
-                <g-sub-nav name="iphone">
-                    <template slot="title">电话</template>
-                    <g-nav-item name="yd">移动</g-nav-item>
-                    <g-nav-item name="lt">联通</g-nav-item>
-                    <g-nav-item name="dx">移动</g-nav-item>
-                </g-sub-nav>
-            </g-sub-nav>
-        </g-nav>
-        <div class="line"></div>
-        <g-nav
-            :default-selected-keys.sync="navSelected1"
-            background-color="#545c64"
-            text-color="#fff"
-            active-text-color="#ffd04b"
-        >
-            <g-nav-item name="1-1">菜单1</g-nav-item>
-            <g-nav-item name="1-2">菜单2</g-nav-item>
-            <g-sub-nav name="1-3">
-                <template slot="title">菜单3</template>
-                <g-nav-item name="2-1-1">子菜单1</g-nav-item>
-                <g-nav-item name="2-1-2">子菜单2</g-nav-item>
-                <g-sub-nav name="2-1-3">
-                    <template slot="title">子菜单3</template>
-                    <g-nav-item name="3-1-1">孙子菜单2</g-nav-item>
-                    <g-nav-item name="3-1-2">孙子菜单3</g-nav-item>
-                    <g-nav-item name="3-1-3">孙子菜单4</g-nav-item>
-                </g-sub-nav>
             </g-sub-nav>
         </g-nav>
     </div>
@@ -42,26 +15,21 @@
 <script>
 import Nav from '../../../../src/nav/nav'
 import NavItem from '../../../../src/nav/nav-item'
-import subNav from '../../../../src/nav/sub-nav'
+import SubNav from '../../../../src/nav/sub-nav'
 export default {
+    name: 'NavDemo1',
     components: {
         gNav: Nav,
         gNavItem: NavItem,
-        gSubNav: subNav
+        gSubNav: SubNav
     },
     data() {
         return {
-            navSelected: ['home'],
-            navSelected1: ['1-1']
+            navSelected: ['home']
         }
     }
 }
 </script>
 
-<style lang="scss" scoped>
-.line {
-    height: 1px;
-    background-color: #e0e6ed;
-    margin: 35px -20px;
-}
+<style lang="scss">
 </style>
--- docs/components/nav/README.md
+++ docs/components/nav/README.md
@@ -1,8 +1,11 @@
 # Nav 导航栏
+<p>为网站提供导航功能的菜单。</p>
 
 ## 顶栏
+<p>适用广泛的基础用法。</p>
 
  <common-demoCode>
+  <nav-demo1 />
    <highlight-code slot="codeText" lang="vue">
     <template>
       <div>123</div>
