703872fdcb8462e32dc4a5e2999c6747364b50d8
liaozhen <zhen.liao@baobeituan.com>
2021-01-12 17:16:29 +0800

    初步完成pagnition组件

--- src/pagination/pagination.vue
+++ src/pagination/pagination.vue
@@ -1,34 +1,81 @@
 <template>
     <div class="g-pagination">
         <span
-            v-for="(page, index) in pages"
-            :key="index"
-            class="g-pagination-item"
-            :class="{ active: currentPage === page }"
-            @click="onClickPage(page)"
-            >{{ page }}</span
+            class="g-pagination-nav"
+            :class="{ disabled: currentPage === 1 }"
+            @click="onClickPage(currentPage - 1)"
         >
+            <g-icon icon-name="left"></g-icon>
+        </span>
+        <template v-for="(page, index) in pages">
+            <template v-if="page === currentPage">
+                <span class="g-pagination-item active" :key="index">{{
+                    page
+                }}</span>
+            </template>
+            <template v-else-if="page === '...'">
+                <span class="g-pagination-item separation" :key="index">
+                    <g-icon icon-name="dot"></g-icon>
+                </span>
+            </template>
+            <template v-else>
+                <span
+                    class="g-pagination-item"
+                    :key="index"
+                    @click="onClickPage(page)"
+                    >{{ page }}</span
+                >
+            </template>
+        </template>
+        <span
+            class="g-pagination-nav"
+            :class="{ disabled: currentPage === totalPage }"
+            @click="onClickPage(currentPage + 1)"
+        >
+            <g-icon icon-name="right"></g-icon>
+        </span>
     </div>
 </template>
 
 <script>
+import gIcon from '../icon/icon'
 export default {
     name: 'Pagination',
     data() {
-        let pages = [
-            1,
-            this.currentPage,
-            this.totalPage,
-            this.currentPage - 1,
-            this.currentPage + 1,
-            this.currentPage - 2,
-            this.currentPage + 2,
-        ]
-        pages = pages.sort((a, b) => a - b)
-        console.log(pages)
-        return {
-            pages,
-        }
+        return {}
+    },
+    computed: {
+        pages() {
+            let unique = (pages) => {
+                const obj = {}
+                pages.forEach((item) => {
+                    obj[item] = true
+                })
+                return Object.keys(obj).map((item) => parseInt(item, 10))
+            }
+            return unique(
+                [
+                    1,
+                    this.totalPage,
+                    this.currentPage,
+                    this.currentPage - 1,
+                    this.currentPage + 1,
+                    this.currentPage - 2,
+                    this.currentPage + 2,
+                ].sort((a, b) => a - b)
+            )
+                .filter((item) => item > 0 && item <= this.totalPage)
+                .reduce((prev, current, index, array) => {
+                    prev.push(current)
+                    if (array[index + 1] && array[index + 1] - current > 1) {
+                        prev.push('...')
+                    }
+                    return prev
+                }, [])
+        },
+    },
+    components: {
+        gIcon,
     },
     props: {
         totalPage: {
@@ -43,8 +90,8 @@ export default {
     methods: {
         onClickPage(page) {
             this.$emit('update:currentPage', page)
-        }
-    }
+        },
+    },
 }
 </script>
 
@@ -66,6 +113,28 @@ export default {
             color: #fff;
             background-color: $primary-color;
             border-color: $primary-color;
+            svg {
+                fill: #fff;
+            }
+        }
+        &.active {
+            cursor: default;
+        }
+    }
+    &-nav {
+        width: 20px;
+        height: 20px;
+        background-color: $bg-color;
+        display: inline-flex;
+        justify-content: center;
+        align-content: center;
+        margin: 0 4px;
+        cursor: pointer;
+        &.disabled {
+            cursor: not-allowed;
+            svg {
+                fill: #c0c4cc;
+            }
         }
     }
 }
